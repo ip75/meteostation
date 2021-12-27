7d16a429c09c622df269cb09040f6cfab6c3544d
Igor <Igor.Polovykh@gmail.com>
2021-12-27 04:31:56 +0000

    logging

--- backend/server/meteostation.go
+++ backend/server/meteostation.go
@@ -5,6 +5,7 @@ import (
 
 	pb "github.com/ip75/meteostation/proto/api"
 	"github.com/ip75/meteostation/storage"
+	log "github.com/sirupsen/logrus"
 )
 
 type MeteostationServer struct {
@@ -16,5 +17,17 @@ func NewMeteostationServer() *MeteostationServer {
 }
 
 func (s *MeteostationServer) GetMeteoData(ctx context.Context, filter *pb.Filter) (*pb.MeteoData, error) {
-	return storage.PG.GetMeteoData(filter)
+	log.Printf("GetMeteoData:\n\tfrom: %s\n\tto: %s\n\tgranularity: %d", filter.From, filter.To, filter.Granularity)
+
+	data, err := storage.PG.GetMeteoData(filter)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"from": filter.From,
+			"to":   filter.To,
+		}).Error(err, "Error when fetching meteodata")
+		return nil, err
+	}
+
+	log.Printf("GetMeteoData: fetched %d records with meteodata", data.TotalCount)
+	return data, err
 }
